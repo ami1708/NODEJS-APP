cd26491698d041d945b0002c2f726e9801b710e5
ami1708 <amisha17200@gmail.com>
2020-08-21 01:08:28 +0530

    'add'

---
+++ controllers/home_controller.js
@@ -0,0 +1,6 @@
+module.exports.home = function(req, res){
+    return res.end('<h1>Express is up for Codial!</h1>')
+}
+
+// module.exports.actionName = function(req, res){}
+//access this in routes go to routes//
--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const port = 8000;
 
+// use express router
+app.use('/', require('./routes'));
 
 
-
-
-app.listen(function(err){
-    if(err){
-        console.log('server is giving error:',err)
+app.listen(port, function(err){
+    if (err){
+        console.log(`Error in running the server: ${err}`);
     }
-    console.log('server is up and running at port:',port)
-})
+
+    console.log(`Server is running on port: ${port}`);
+});
