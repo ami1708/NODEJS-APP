8d6d5da37636505f30dc546e55268e07024f73eb
ami1708 <amisha17200@gmail.com>
2020-08-21 02:16:08 +0530

    'added'

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,9 @@
-module.exports.home = function(req, res){
-    return res.end('<h1>Express is up for Codial!</h1>')
+module.exports.home = function(req,res){
+    return res.render('home',{
+        title: "home"
+    });
 }
+
 
 // module.exports.actionName = function(req, res){}
 //access this in routes go to routes//
--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const app = express();
 const port = 8000;
-
+//requiring the layouts
+const expressLayouts = require('express-ejs-layouts')
+//use layouts
+app.use(expressLayouts);
 // use express router
+
+//use static files
+app.use(express.static('./assets'));
+// /connecting to database
+const db = require('./config/mongoose');
+
+//using routes
 app.use('/', require('./routes'));
+//use view engine
+app.set('view engine','ejs')
+app.set('views','./views')
 
 
 app.listen(port, function(err){
     if (err){
         console.log(`Error in running the server: ${err}`);
     }
+
 
     console.log(`Server is running on port: ${port}`);
 });
